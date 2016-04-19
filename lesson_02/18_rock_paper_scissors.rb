af5c8bbc313f0988db293ab073095877e22170f1
bcasadei <bill@billcasadei.com>
2016-04-18 22:08:37 -0600

    Added Rock Paper Scissors

---
+++ lesson_02/18_rock_paper_scissors.rb
@@ -0,0 +1,48 @@
+# 18_rock_paper_scissors.rb
+
+VALID_CHOICES = %w('rock', 'paper', 'scissors').freeze
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def display_results(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do # main loop
+choice = ''
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+  choice = gets.chomp
+
+  if VALID_CHOICES.include?(choice)
+    break
+  else
+    prompt("That's not a valid choice.")
+  end
+end
+
+computer_choice = VALID_CHOICES.sample
+
+prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+
+display_results(choice, computer_choice)
+
+prompt("Do you want to play again? (Y/N)")
+answer = gets.chomp
+break unless answer.downcase.start_with?('y')
+end # end main loop
+
+prompt("Thank you for playing Rock, Paper, Scissors!")
+ 
