25747466ba3873421dc28820adede2bc24b35907
bcasadei <bill@billcasadei.com>
2016-04-20 17:59:06 -0600

    refactored

--- lesson_02/18_rock_paper_scissors.rb
+++ lesson_02/18_rock_paper_scissors.rb
@@ -1,19 +1,21 @@
 # 18_rock_paper_scissors.rb
 
-VALID_CHOICES = %w('rock', 'paper', 'scissors').freeze
+VALID_CHOICES = %w(rock paper scissors).freeze
 
 def prompt(message)
   puts("=> #{message}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
@@ -21,28 +23,27 @@ def display_results(player, computer)
 end
 
 loop do # main loop
-choice = ''
-loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = gets.chomp
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
 
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice.")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
   end
-end
 
-computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.sample
 
-prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
 
-display_results(choice, computer_choice)
+  display_results(choice, computer_choice)
 
-prompt("Do you want to play again? (Y/N)")
-answer = gets.chomp
-break unless answer.downcase.start_with?('y')
+  prompt("Do you want to play again? (Y/N)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end # end main loop
 
 prompt("Thank you for playing Rock, Paper, Scissors!")
- 
--- lesson_02/21_rps_bonus.rb
+++ lesson_02/21_rps_bonus.rb
@@ -1,6 +1,8 @@
 # 21_rps_bonus.rb
 
 VALID_CHOICES = %w(r p s l sp).freeze
+p_score = 0
+cpu_score = 0
 
 def prompt(message)
   puts("=> #{message}")
@@ -8,88 +10,69 @@ end
 
 def win?(first, second)
   (first == 'r' && second == 's') ||
-  (first == 'p' && second == 'r') ||
-  (first == 's' && second == 'p') ||
-  (first == 'l' && second == 'p') ||
-  (first == 'sp' && second == 's') ||
-  (first == 'r' && second == 'l') ||
-  (first == 'p' && second == 'sp') ||
-  (first == 's' && second == 'l') ||
-  (first == 'l' && second == 'sp') ||
-  (first == 'sp' && second == 'r')
+    (first == 'p' && second == 'r') ||
+    (first == 's' && second == 'p') ||
+    (first == 'l' && second == 'p') ||
+    (first == 'sp' && second == 's') ||
+    (first == 'r' && second == 'l') ||
+    (first == 'p' && second == 'sp') ||
+    (first == 's' && second == 'l') ||
+    (first == 'l' && second == 'sp') ||
+    (first == 'sp' && second == 'r')
 end
 
 def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
-    score_keeper(player)
   elsif win?(computer, player)
     prompt("Computer won!")
-    score_keeper(computer)
   else
     prompt("It's a tie!")
   end
 end
 
-def score_keeper(point)
-  case point
-  when 'player'
-    p_score += 1
-  when 'computer'
-    cpu_score += 1
-  end
-end
-
-def check_winner(score1, score2)
-  if score1 == 5
-    prompt("You won the match! Do you want to play again? (Y/N)")
-    answer = gets.chomp
-    reset score
-  elsif score2 == 5
-    prompt("Computer wins the match! Do you want to play again? (Y/N)")
-    answer = gets.chomp
-    reset_score
-  end
-end
-
-def reset_score
-  p_score = 0
-  cpu_score = 0
-end
+welcome_prompt = <<-MSG
+  ----------------------------------
+  Welcome to Rock, Paper, Scissors, Spock!
+  ----------------------------------------
+  Scissors cuts Paper covers Rock crushes
+  Lizard poisons Spock smashes Scissors
+  decapitates Lizard eats Paper disproves
+  Spock vaporizes Rock crushes Scissors.
+  ----------------------------------------
+  First to five wins the match!
+  ----------------------------------------
+  MSG
+prompt(welcome_prompt)
 
 loop do # main loop
-  p_score = 0
-  cpu_score = 0
   choice = ''
 
   loop do
-    welcome_prompt = <<-MSG
-    ----------------------------------
-    Welcome to Rock, Paper, Scissors, Spock!
-    ----------------------------------------
-    Scissors cuts Paper covers Rock crushes
-    Lizard poisons Spock smashes Scissors
-    decapitates Lizard eats Paper disproves
-    Spock vaporizes Rock crushes Scissors.
-    ----------------------------------------
-    First to five wins the match!
-    ----------------------------------------
-    MSG
-    prompt(welcome_prompt)
-
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    break if VALID_CHOICES.include?(choice)
+    prompt("That's not a valid choice.")
   end
 
   computer_choice = VALID_CHOICES.sample
 
+  if win?(choice, computer_choice)
+    p_score += 1
+  elsif win?(computer_choice, choice)
+    cpu_score += 1
+  end
+
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+
+  if p_score == 5
+    prompt("You won the match! You: #{p_score} Computer: #{cpu_score}.")
+    break
+  elsif cpu_score == 5
+    prompt("The computer won the match! You: #{p_score} Computer: #{cpu_score}.")
+    break
+  end
 
   display_results(choice, computer_choice)
 
@@ -98,4 +81,4 @@ loop do # main loop
   break unless answer.downcase.start_with?('y')
 end # end main loop
 
-prompt("Thank you for playing Rock, Paper, Scissors, Lizard, Spock!")
+prompt("Thank you for playing Rock, Paper, Scissors, Lizard, Spock")
